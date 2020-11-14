72a2d4266109af96e4778bd9de019012cc8cdc80
Leah Brady <brady.leah@gmail.com>
2020-11-14 18:15:57 -0500

    add SVG wrapper

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,9 @@ var margin = {
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3
+  .select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
